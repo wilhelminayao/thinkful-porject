911f4f8791c3b90a3250ad7871649f098004efa7
Ziwei Yao <zyao@noodle.org>
2014-02-24 15:52:58 -0500

    flask app project

---
+++ flask app/microblog/app/views.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template,flash,redirect
+from forms import LoginForm
+'''
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        {
+            'author' : {'nickname' : 'John'},
+            'body' : 'Beautiful day in Porland!'
+        },
+        {'author' : {'nickname' : 'Susan'},
+         'body' : 'The Avengers movie was so cool!'}
+    ]
+    return render_template("index,html",title = 'Home', user = user,posts = posts)
+
+    '''
+@app.route('/login',method = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' +str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',title = 'Sign In',form = form,providers = app.config['OPENID_PROVIDERS'])
