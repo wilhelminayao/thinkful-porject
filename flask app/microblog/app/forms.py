ad03c5615b993815a63e4c6fd932c823ac44d8df
Ziwei Yao <zyao@noodle.org>
2014-02-25 09:28:58 -0500

    flask app project

--- flask app/microblog/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BlooleanField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
-    remember_me = BlooleanField('remember_me',default = False)
